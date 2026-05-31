63d201609acd6f6a38614b41d9fd75f3ba45f95f
Rido Rinaldi <redorenaldi99@gmail.com>
2026-05-31 12:03:25 +0700

    fix deploy config and restore styling

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { cloudflare } from '@cloudflare/vite-plugin'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
+import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import { fileURLToPath, URL } from "node:url"
 
 export default defineConfig({
   plugins: [
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tanstackStart(),
     react(),
+    tailwindcss(),
   ],
 
   resolve: {
-  alias: {
-    "@": fileURLToPath(new URL("./src", import.meta.url)),
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
-},
 })
